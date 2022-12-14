3c52000756afacb6dc130bdc15a1d2efed4da173
Brice Vandeputte <brice.vandeputte@orange.com>
2022-12-14 14:05:01 +0100

    improve mocha example with both logtail packages

---
+++ tests/mochaSampleLogtailJs.js
@@ -0,0 +1,24 @@
+import { strict as assert } from 'assert';
+import { expect } from 'chai';
+import util from 'util';
+import { Node } from "@logtail/js";
+const isSet = (value) => (value !== null && value !== undefined);
+
+const logtailSource = process.env.CH_LOGTAIL_SOURCE;
+
+describe("@logtail/js + Mocha", function() {
+
+    before(async function () {
+      console.info(" :: before");
+      if (!isSet(logtailSource)) {
+        console.log("please set CH_LOGTAIL_SOURCE");
+        process.exit(1);
+      }
+    });
+
+    it("should work", function() {
+        const logtail = new Node(logtailSource);
+        logtail.info("Logtail @logtail/js tested " + new Date());
+    });
+
+});
--- tests/mochaSampleLogtailNode.js
+++ tests/mochaSampleLogtailNode.js
@@ -6,7 +6,7 @@ const isSet = (value) => (value !== null && value !== undefined);
 
 const logtailSource = process.env.CH_LOGTAIL_SOURCE;
 
-describe("logTail + Mocha", function() {
+describe("@logtail/node + Mocha", function() {
 
     before(async function () {
       console.info(" :: before");
@@ -18,7 +18,7 @@ describe("logTail + Mocha", function() {
 
     it("should work", function() {
         const logtail = new Logtail(logtailSource);
-        logtail.info("Logtail tested " + new Date());
+        logtail.info("Logtail @logtail/node tested " + new Date());
     });
 
 });
